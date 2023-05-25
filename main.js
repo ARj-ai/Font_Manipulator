0b1fe04c8a9ae6274a8a95b82bf8a72527b76e64
ARj-ai <131518958+ARj-ai@users.noreply.github.com>
2023-05-25 18:57:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+    pose_net = ml5.poseNet(video, modelLoaded);
+    pose_net.on('pose', got_poses);
+}
+
+function modelLoaded(){
+    console.log("Pose net is intialised");
+}
+
+function got_poses(results){
+    if(results.length > 0){
+        console.log(results);
+    }
+}
