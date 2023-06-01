af30837652f00be1174b529a64e9195c1f5c4a4e
ARj-ai <131518958+ARj-ai@users.noreply.github.com>
2023-06-01 14:36:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
+lwrist_x = 0;
+rwrist_x = 0;   
+difference = 0;
+
 function setup(){
     video = createCapture(VIDEO);
     video.size(550, 500);
     canvas = createCanvas(500, 500);
     canvas.position(700, 150);
-    background("white");
     pose_net = ml5.poseNet(video, modelLoaded);
     pose_net.on('pose', got_poses);
 }
@@ -15,5 +18,18 @@ function modelLoaded(){
 function got_poses(results){
     if(results.length > 0){
         console.log(results);
+        lwrist_x  = results[0].pose.leftWrist.x;
+        rwrist_x = results[0].pose.rightWrist.x;
+        difference = floor(lwrist_x - rwrist_x);
+        console.log("lwrist_x = " + results[0].pose.leftWrist.x + "leftwrist_y = " + results[0].pose.leftWrist.y);
+        console.log("rightwrist_x = " + results[0].pose.rightWrist.x + "rightwrist_y = " + results[0].pose.rightWrist.y);
     }
+}
+
+function draw(){
+    background("white");
+    textSize(difference);
+    fill(255, 0, 0);
+    text('Arjun', 50, 400);
+    document.getElementById("font_size").innerHTML = "Font size of the text will be = " + difference +"px";
 }
